73b9a61c484eb92bd16335a67aa4d396a77f03da
Aya1996syuger <ayaka.coco87@gmail.com>
2021-11-08 13:20:00 +0000

    bootstrapレイアウト調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,17 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_permitted_parameters
+def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    @user = User.new
+  if @user.save
+      flash[:success]
+      redirect_to user_path
   end
+end
 
   def after_sign_in_path_for(resource)
+
     user_path(resource)
   end
 end
