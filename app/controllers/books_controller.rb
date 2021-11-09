5ed00152eadc7da87002855e1e3a382a17fcd788
Aya1996syuger <ayaka.coco87@gmail.com>
2021-11-09 12:22:10 +0000

    テスト中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,9 +33,14 @@ class BooksController < ApplicationController
   end
 
   def update
-   book = Book.find(params[:id])
-   book.update(book_params)
-   redirect_to book_path(book.id)
+     book = Book.find(params[:id])
+     if book.update(book_params)
+         flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(book.id)
+     else
+      @books = Book.all
+      render :edit
+     end
   end
 
   def destroy
